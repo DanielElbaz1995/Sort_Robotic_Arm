d376c08f753f79e5c2c55d55343f9e45f3ba8477
DanielElbaz1995 <danielelbaz1995@gmail.com>
2025-03-10 17:54:53 +0200

    Delete src/sort_robotic_arm_gazebo/src/main_object_detector.cpp

--- src/sort_robotic_arm_gazebo/src/main_object_detector.cpp
+++
@@ -1,9 +0,0 @@
-#include "object_detector.hpp"
-
-int main(int argc, char** argv) {
-    rclcpp::init(argc, argv); // Initializes the ROS 2 system.
-    auto node = std::make_shared<ObjectDetectorNode>(); 
-    rclcpp::spin(node);  
-    rclcpp::shutdown();
-    return 0;
-}
